bc187fbd99934f542f3c42688110ff71c84e28b3
danielgarciadasilva <daniel.garciadasilva3@gmail.com>
2026-04-23 22:00:48 -0300

    exercicios de classe

--- src/POO/Basico/exercicios/PrincipalContaBancaria.java
+++ src/POO/Basico/exercicios/PrincipalContaBancaria.java
@@ -5,7 +5,7 @@ public class PrincipalContaBancaria {
         ContaBancaria cb = new ContaBancaria(100);
 
         System.out.println(cb.sacar(20));
-        System.out.println(cb.sacar(-50));
+        System.out.println(cb.sacar(20));
         System.out.println(cb.sacar(0));
         System.out.println(cb.sacar(10_000));
 
